d6e1324ab2323a7b4b12803579bd2b0ce923caa8
https://git.rnp.br/vivian <vivian@gta.ufrj.br>
2025-03-29 19:13:23 -0300

    inicializando

---
+++ trabalho1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <limits>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
